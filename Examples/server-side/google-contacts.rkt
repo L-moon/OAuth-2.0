c649993a0dca8ff4d2e10da5ebf3157b2a3bc176
veer <diggerrrrr@gmail.com>
2011-08-30 16:51:03 +0530

    Made the example simpler by moving authorization part to different module.
    Removed get-authorization , we now import this from another module.
    Made some minor change in get-contacts to reflect above effect.

--- Examples/server-side/google-contacts.rkt
+++ Examples/server-side/google-contacts.rkt
@@ -1,23 +1,24 @@
 #lang racket
 (require web-server/servlet
          web-server/servlet-env)
-(require net/url net/uri-codec)
-(require (file "../../utils/web-helper.rkt"))
 (require (file "../../oauth-2.rkt"))
-(require (prefix-in private: (file "../../../secret/secret.rkt")))
+(require "authorization-handler.rkt")
 
-(define encode form-urlencoded-encode)
+;;"secret.rkt" is only used only to export oauth-obj
+;; comment this and create your own oauth-obj as shown below.
+(require (prefix-in private: (file "../../../secret/secret.rkt"))) 
 
 ;;import oauth-obj from some place for example secret.rkt OR
 (define oauth-obj private:oauth-obj)
 ;;Create your own using make-oauth-2 as shown below.
 ;  (make-oauth-2
-;   #:client-id "abc .... blah"
-;   #:client-secret "45bg......"
+;   #:client-id "Your  client ID"
+;   #:client-secret "Your client secret"
 ;   #:authorization-uri "https://accounts.google.com/o/oauth2/auth"
 ;   #:token-uri "https://accounts.google.com/o/oauth2/token"
 ;   #:redirect-uri "http://localhost:8000/oauth2callback.rkt"
 ;   #:response-type 'code))
+
 
 ;;list of services required
 (define scope (list "https://www.google.com/m8/feeds"))
@@ -35,57 +36,14 @@
   
   (define (get-contacts request)
     
-    (define json-obj (get-authorization oauth-obj))
-    (define contacts (get-all-contacts (hash-ref json-obj 'access_token)))
+    (define access-token (get-authorization oauth-obj scope))
+    (define contacts (get-all-contacts access-token))
     (begin
       (printf "~a\n" contacts)
       (response/xexpr
        `(html (body (h1 "Success in retrieving contacts"))))))
-  
-  
+    
   (send/suspend/dispatch gen-resp))
-
-;;Main function for authorization and access token.
-(define (get-authorization oauth-obj)
-  
-  ;;Need to check if we are already authorized ,
-  ;;may be using cookie??
-  
-  (define (get-code bindings)
-    (if (binding-exists? 'code bindings)
-        (get-value 'code bindings)
-        #f))
-  
-  (define (get-error bindings)
-    (if (binding-exists? 'error bindings)
-        (get-value 'error bindings)
-        #f))
-  
-  (define req (send/suspend
-               (lambda (make-url)
-                 (redirect-to 
-                  (request-owner-for-grant oauth-obj 
-                                           #:state (encode make-url)
-                                           #:scope scope)))))
-  
-  (define bindings (get-bindings req))
-  (define code (get-code bindings))
-  ;(define error (get-error bindings))
-  
-  (if code
-      
-      (let ([json-obj (request-access-token oauth-obj #:code code)])
-        (if (hash-ref json-obj 'error #f)
-            
-            (send/back
-            (response/xexpr
-             `(html (body (h1 ,(hash-ref json-obj 'error))))))
-            
-            json-obj))
-      
-      (send/back (response/xexpr
-                  `(html (body (h1 "Error: " ,(or (get-error bindings)
-                                                  "unknown error"))))))))
 
 
 
@@ -94,7 +52,6 @@
     (set-url-query! url (list (cons 'access_token access-token)))
     (port->bytes (get-pure-port url
                                 (list "GData-Version: 3.0")))))
-
 
 (serve/servlet start #:servlet-path "/"
                #:servlets-root (current-directory))

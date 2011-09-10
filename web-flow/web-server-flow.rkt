6a86aa9e050363a9c1146a02903cb264cd889073
veer <diggerrrrr@gmail.com>
2011-09-10 11:37:36 +0530

    Adds support for stateless servlets. Exports stateless:get-authorization-web-flow

--- web-flow/web-server-flow.rkt
+++ web-flow/web-server-flow.rkt
@@ -1,9 +1,11 @@
 #lang racket
-(require "flow.rkt"
+(require "flow.rkt"         
          "callback.rkt")
+(require (prefix-in stateless: "stateless-flow.rkt"))
 (require (file "../oauth-2.rkt"))
          
 
-(provide (all-from-out (file "../oauth-2.rkt"))
+(provide (all-from-out (file "../oauth-2.rkt"))         
  get-authorization-web-flow
+ stateless:get-authorization-web-flow
  may-be-callback)
